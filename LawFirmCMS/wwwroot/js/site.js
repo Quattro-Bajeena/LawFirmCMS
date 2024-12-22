b28fee63e80dc409ce87bc95e1016e6dc563296c
Patrycja Przybylska <patr.przybylska@gmail.com>
2024-12-22 20:17:47 +0100

    js - nie dziala

--- LawFirmCMS/wwwroot/js/site.js
+++ LawFirmCMS/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var tabItems = document.querySelectorAll('.tab-item');
+
+// Dodajemy nasłuchiwacz zdarzeń na każdy element
+tabItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+        // Usuwamy klasę 'active' ze wszystkich elementów
+        tabItems.forEach(function (link) {
+            link.classList.remove('active');
+        });
+
+        // Dodajemy klasę 'active' do klikniętego elementu
+        item.classList.add('active');
+    });
+});
